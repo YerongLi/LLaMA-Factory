f72c3a55b0a8572e86adb87941d5e03ca30285c4
Yerong <yerong.li@outlook.com>
2023-12-11 16:51:55 -0600

    Print the history

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
 python src/cli_eval.py \
     --model_name_or_path /scratch/yerong/.cache/pyllama/Llama-2-7b-hf \
-    --template default \
+    --template dispatcher \
     --finetuning_type lora \
     --checkpoint_dir police2/checkpoint-4410
