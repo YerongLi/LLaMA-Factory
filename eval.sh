f265b96f542b4336d026d208d96ca892eeda7fd7
Yerong <yerong.li@outlook.com>
2023-12-11 06:35:42 -0600

    Upload the evaluation script

--- eval.sh
+++ eval.sh
@@ -2,5 +2,4 @@ python src/cli_eval.py \
     --model_name_or_path /scratch/yerong/.cache/pyllama/Llama-2-7b-hf \
     --template default \
     --finetuning_type lora \
-    --checkpoint_dir police4/checkpoint-10 \
-    --quantization_bit 4
+    --checkpoint_dir police1
