b7e08be2bb22af9ed80d5afcb764eb8f788f7e3d
Yerong <yerong.li@outlook.com>
2023-12-11 16:01:44 -0600

    Use checkpoint-430

--- eval.sh
+++ eval.sh
@@ -2,4 +2,4 @@ python src/cli_eval.py \
     --model_name_or_path /scratch/yerong/.cache/pyllama/Llama-2-7b-hf \
     --template default \
     --finetuning_type lora \
-    --checkpoint_dir police2/checkpoint-430
+    --checkpoint_dir police2/checkpoint-4410
