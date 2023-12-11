d552c438a1ebe293b078874e7f3d730cb3a47b04
Yerong <yerong.li@outlook.com>
2023-12-11 03:34:13 -0600

    Upload demo.sh script

---
+++ demo.sh
@@ -0,0 +1,5 @@
+python src/cli_demo.py \
+    --model_name_or_path /scratch/yerong/.cache/pyllama/Llama-2-7b-hf \
+    --template default \
+    --finetuning_type lora \
+    --checkpoint_dir police4/checkpoint-10
