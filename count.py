87d26d3ec53c7bc286f83da988f5c0a20d71f925
Yerong <yerong.li@outlook.com>
2023-12-14 09:14:56 -0600

    Upload count

--- count.py
+++ count.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from sklearn.metrics import f1_score, confusion_matrix
 
 # Specify the directory where the CSV files are located
 directory_path = 'out'
@@ -25,6 +26,18 @@ for filename in os.listdir(directory_path):
             'r_tone': (df['r_tone'] < 30).sum(),
             'o_tone': (df['o_tone'] < 30).sum()
         }
+        df = pd.read_csv(file_path)
+
+        # Define conditions for positive, negative, and neutral
+        positive_condition = df['o_tone'] > 70
+        negative_condition = df['o_tone'] < 30
+
+        # Assign true labels based on conditions
+        true_labels.extend([1 if pos else (-1 if neg else 0) for pos, neg in zip(positive_condition, negative_condition)])
+
+        # Assign predicted labels based on 'r_tone' predictions
+        predicted_labels.extend([1 if val > 70 else (-1 if val < 30 else 0) for val in df['r_tone']])
+        f1 = f1_score(true_labels, predicted_labels, average='weighted')
 
         # Print the results for each file
         print(f"File: {filename}")
@@ -35,4 +48,6 @@ for filename in os.listdir(directory_path):
         print(f"Count of 'i_tone' values below 30: {count_below_30['i_tone']}")
         print(f"Count of 'r_tone' values below 30: {count_below_30['r_tone']}")
         print(f"Count of 'o_tone' values below 30: {count_below_30['o_tone']}")
+        print(f"F1 Score: {f1}")
+
         print("\n")
