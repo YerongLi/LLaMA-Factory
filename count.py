42d647fda2afe8c6f1742e48f0d33c747e8f534c
Yerong <yerong.li@outlook.com>
2023-12-14 09:23:24 -0600

    Update count.py

--- count.py
+++ count.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import os
+
+# Specify the directory where the CSV files are located
+directory_path = 'out'
+
+# Loop through each CSV file in the directory
+import pandas as pd
+import os
 from sklearn.metrics import f1_score, confusion_matrix
 
 # Specify the directory where the CSV files are located
 directory_path = 'out'
 
+# Specify the file to append F1 scores
+
+# Open the file in append mode
 # Loop through each CSV file in the directory
 for filename in os.listdir(directory_path):
     if filename.endswith('.csv'):
@@ -26,20 +36,8 @@ for filename in os.listdir(directory_path):
             'r_tone': (df['r_tone'] < 30).sum(),
             'o_tone': (df['o_tone'] < 30).sum()
         }
-        df = pd.read_csv(file_path)
 
-        # Define conditions for positive, negative, and neutral
-        positive_condition = df['o_tone'] > 70
-        negative_condition = df['o_tone'] < 30
-
-        # Assign true labels based on conditions
-        true_labels.extend([1 if pos else (-1 if neg else 0) for pos, neg in zip(positive_condition, negative_condition)])
-
-        # Assign predicted labels based on 'r_tone' predictions
-        predicted_labels.extend([1 if val > 70 else (-1 if val < 30 else 0) for val in df['r_tone']])
-        f1 = f1_score(true_labels, predicted_labels, average='weighted')
-
-        # Print the results for each file
+        # Print the original counts for each file
         print(f"File: {filename}")
         print(f"Count of 'i_tone' values above 70: {count_above_70['i_tone']}")
         print(f"Count of 'r_tone' values above 70: {count_above_70['r_tone']}")
@@ -48,6 +46,23 @@ for filename in os.listdir(directory_path):
         print(f"Count of 'i_tone' values below 30: {count_below_30['i_tone']}")
         print(f"Count of 'r_tone' values below 30: {count_below_30['r_tone']}")
         print(f"Count of 'o_tone' values below 30: {count_below_30['o_tone']}")
-        print(f"F1 Score: {f1}")
+        
+        # Define conditions for positive, negative, and neutral
+        positive_condition = df['o_tone'] > 70
+        negative_condition = df['o_tone'] < 30
+
+        # Assign true labels based on conditions
+        true_labels = [1 if pos else (-1 if neg else 0) for pos, neg in zip(positive_condition, negative_condition)]
+
+        # Assign predicted labels based on 'r_tone' predictions
+        predicted_labels = [1 if val > 70 else (-1 if val < 30 else 0) for val in df['r_tone']]
+
+        # Calculate F1 score for the current file
+        f1 = f1_score(true_labels, predicted_labels, average='weighted')
+
+        # Print the F1 score for the current file
+        print(f"F1 Score for {filename}: {f1}")
+
+        # Append the results to the output file
 
         print("\n")
