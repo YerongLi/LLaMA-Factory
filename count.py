0f580e41bd72908140e68bce0dfa999765dfb788
Yerong <yerong.li@outlook.com>
2023-12-14 09:44:21 -0600

    Update count.py

--- count.py
+++ count.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import os
-from sklearn.metrics import f1_score, confusion_matrix
+from sklearn.metrics import f1_score
 
 # Specify the directory where the CSV files are located
 directory_path = 'out'
 
 # Specify the file to append F1 scores
 output_file = 'f1_scores.txt'
+
+# Specify upper and lower bounds for neutral
+lower_bound = 50
+upper_bound = 50
 
 # Open the file in append mode
 with open(output_file, 'a') as f:
@@ -34,9 +38,9 @@ with open(output_file, 'a') as f:
 
             # Count occurrences where 'i_tone', 'r_tone', and 'o_tone' are between 30 and 70
             count_neutral = {
-                'i_tone': ((df['i_tone'] >= 30) & (df['i_tone'] <= 70)).sum(),
-                'r_tone': ((df['r_tone'] >= 30) & (df['r_tone'] <= 70)).sum(),
-                'o_tone': ((df['o_tone'] >= 30) & (df['o_tone'] <= 70)).sum()
+                'i_tone': ((df['i_tone'] >= lower_bound) & (df['i_tone'] <= upper_bound)).sum(),
+                'r_tone': ((df['r_tone'] >= lower_bound) & (df['r_tone'] <= upper_bound)).sum(),
+                'o_tone': ((df['o_tone'] >= lower_bound) & (df['o_tone'] <= upper_bound)).sum()
             }
 
             # Print the original counts for each file
@@ -49,9 +53,9 @@ with open(output_file, 'a') as f:
             print(f"Count of 'r_tone' values below 30: {count_below_30['r_tone']}")
             print(f"Count of 'o_tone' values below 30: {count_below_30['o_tone']}")
 
-            print(f"Count of 'i_tone' values between 30 and 70 (neutral): {count_neutral['i_tone']}")
-            print(f"Count of 'r_tone' values between 30 and 70 (neutral): {count_neutral['r_tone']}")
-            print(f"Count of 'o_tone' values between 30 and 70 (neutral): {count_neutral['o_tone']}")
+            print(f"Count of 'i_tone' values between {lower_bound} and {upper_bound} (neutral): {count_neutral['i_tone']}")
+            print(f"Count of 'r_tone' values between {lower_bound} and {upper_bound} (neutral): {count_neutral['r_tone']}")
+            print(f"Count of 'o_tone' values between {lower_bound} and {upper_bound} (neutral): {count_neutral['o_tone']}")
 
             # Define conditions for positive, negative, and neutral
             positive_condition_o_tone = df['o_tone'] > 70
@@ -99,6 +103,4 @@ with open(output_file, 'a') as f:
 
             # Print the additional counts
             print(f"Count where 'i_tone' is negative while 'o_tone' is positive: {count_negative_i_tone_positive_o_tone}")
-            print(f"Count where 'i_tone' is negative while 'r_tone' is positive: {count_negative_i_tone_positive_r_tone}")
-
-            print("\n")
+            print(f"Count where 'i_tone' is negative while 'r_tone' is positive: {count_negative_i_tone_positive_r_tone
