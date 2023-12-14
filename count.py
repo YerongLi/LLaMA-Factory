2ced6382a195aa1d0395504423a0d1fe2a430c07
Yerong <yerong.li@outlook.com>
2023-12-14 07:59:37 -0600

    Upload count

--- count.py
+++ count.py
@@ -4,10 +4,6 @@ import os
 # Specify the directory where the CSV files are located
 directory_path = 'out'
 
-# Initialize counters
-count_greater_than_60 = {'i_tone': 0, 'r_tone': 0, 'o_tone': 0}
-count_smaller_than_40 = {'i_tone': 0, 'r_tone': 0, 'o_tone': 0}
-
 # Loop through each CSV file in the directory
 for filename in os.listdir(directory_path):
     if filename.endswith('.csv'):
@@ -17,20 +13,26 @@ for filename in os.listdir(directory_path):
         df = pd.read_csv(file_path)
 
         # Count occurrences where 'i_tone', 'r_tone', and 'o_tone' are greater than 60
-        count_greater_than_60['i_tone'] += (df['i_tone'] > 60).sum()
-        count_greater_than_60['r_tone'] += (df['r_tone'] > 60).sum()
-        count_greater_than_60['o_tone'] += (df['o_tone'] > 60).sum()
+        count_greater_than_60 = {
+            'i_tone': (df['i_tone'] > 60).sum(),
+            'r_tone': (df['r_tone'] > 60).sum(),
+            'o_tone': (df['o_tone'] > 60).sum()
+        }
 
         # Count occurrences where 'i_tone', 'r_tone', and 'o_tone' are smaller than 40
-        count_smaller_than_40['i_tone'] += (df['i_tone'] < 40).sum()
-        count_smaller_than_40['r_tone'] += (df['r_tone'] < 40).sum()
-        count_smaller_than_40['o_tone'] += (df['o_tone'] < 40).sum()
+        count_smaller_than_40 = {
+            'i_tone': (df['i_tone'] < 40).sum(),
+            'r_tone': (df['r_tone'] < 40).sum(),
+            'o_tone': (df['o_tone'] < 40).sum()
+        }
 
-# Print the results
-print(f"Count of 'i_tone' values greater than 60: {count_greater_than_60['i_tone']}")
-print(f"Count of 'r_tone' values greater than 60: {count_greater_than_60['r_tone']}")
-print(f"Count of 'o_tone' values greater than 60: {count_greater_than_60['o_tone']}")
+        # Print the results for each file
+        print(f"File: {filename}")
+        print(f"Count of 'i_tone' values greater than 60: {count_greater_than_60['i_tone']}")
+        print(f"Count of 'r_tone' values greater than 60: {count_greater_than_60['r_tone']}")
+        print(f"Count of 'o_tone' values greater than 60: {count_greater_than_60['o_tone']}")
 
-print(f"Count of 'i_tone' values smaller than 40: {count_smaller_than_40['i_tone']}")
-print(f"Count of 'r_tone' values smaller than 40: {count_smaller_than_40['r_tone']}")
-print(f"Count of 'o_tone' values smaller than 40: {count_smaller_than_40['o_tone']}")
+        print(f"Count of 'i_tone' values smaller than 40: {count_smaller_than_40['i_tone']}")
+        print(f"Count of 'r_tone' values smaller than 40: {count_smaller_than_40['r_tone']}")
+        print(f"Count of 'o_tone' values smaller than 40: {count_smaller_than_40['o_tone']}")
+        print("\n")
