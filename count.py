e2f93969b36720eb6980ceafcfa2bcfe3af241d2
Yerong <yerong.li@outlook.com>
2023-12-14 09:46:47 -0600

    Update count.py

--- count.py
+++ count.py
@@ -22,21 +22,21 @@ with open(output_file, 'a') as f:
             # Read the CSV file into a pandas DataFrame
             df = pd.read_csv(file_path)
 
-            # Count occurrences where 'i_tone', 'r_tone', and 'o_tone' are above 70
-            count_above_70 = {
-                'i_tone': (df['i_tone'] > 70).sum(),
-                'r_tone': (df['r_tone'] > 70).sum(),
-                'o_tone': (df['o_tone'] > 70).sum()
+            # Count occurrences where 'i_tone', 'r_tone', and 'o_tone' are above upper_bound
+            count_above_upper_bound = {
+                'i_tone': (df['i_tone'] > upper_bound).sum(),
+                'r_tone': (df['r_tone'] > upper_bound).sum(),
+                'o_tone': (df['o_tone'] > upper_bound).sum()
             }
 
-            # Count occurrences where 'i_tone', 'r_tone', and 'o_tone' are below 30
-            count_below_30 = {
-                'i_tone': (df['i_tone'] < 30).sum(),
-                'r_tone': (df['r_tone'] < 30).sum(),
-                'o_tone': (df['o_tone'] < 30).sum()
+            # Count occurrences where 'i_tone', 'r_tone', and 'o_tone' are below lower_bound
+            count_below_lower_bound = {
+                'i_tone': (df['i_tone'] < lower_bound).sum(),
+                'r_tone': (df['r_tone'] < lower_bound).sum(),
+                'o_tone': (df['o_tone'] < lower_bound).sum()
             }
 
-            # Count occurrences where 'i_tone', 'r_tone', and 'o_tone' are between 30 and 70
+            # Count occurrences where 'i_tone', 'r_tone', and 'o_tone' are between lower_bound and upper_bound
             count_neutral = {
                 'i_tone': ((df['i_tone'] >= lower_bound) & (df['i_tone'] <= upper_bound)).sum(),
                 'r_tone': ((df['r_tone'] >= lower_bound) & (df['r_tone'] <= upper_bound)).sum(),
@@ -45,23 +45,23 @@ with open(output_file, 'a') as f:
 
             # Print the original counts for each file
             print(f"File: {filename}")
-            print(f"Count of 'i_tone' values above 70: {count_above_70['i_tone']}")
-            print(f"Count of 'r_tone' values above 70: {count_above_70['r_tone']}")
-            print(f"Count of 'o_tone' values above 70: {count_above_70['o_tone']}")
+            print(f"Count of 'i_tone' values above {upper_bound}: {count_above_upper_bound['i_tone']}")
+            print(f"Count of 'r_tone' values above {upper_bound}: {count_above_upper_bound['r_tone']}")
+            print(f"Count of 'o_tone' values above {upper_bound}: {count_above_upper_bound['o_tone']}")
 
-            print(f"Count of 'i_tone' values below 30: {count_below_30['i_tone']}")
-            print(f"Count of 'r_tone' values below 30: {count_below_30['r_tone']}")
-            print(f"Count of 'o_tone' values below 30: {count_below_30['o_tone']}")
+            print(f"Count of 'i_tone' values below {lower_bound}: {count_below_lower_bound['i_tone']}")
+            print(f"Count of 'r_tone' values below {lower_bound}: {count_below_lower_bound['r_tone']}")
+            print(f"Count of 'o_tone' values below {lower_bound}: {count_below_lower_bound['o_tone']}")
 
             print(f"Count of 'i_tone' values between {lower_bound} and {upper_bound} (neutral): {count_neutral['i_tone']}")
             print(f"Count of 'r_tone' values between {lower_bound} and {upper_bound} (neutral): {count_neutral['r_tone']}")
             print(f"Count of 'o_tone' values between {lower_bound} and {upper_bound} (neutral): {count_neutral['o_tone']}")
 
             # Define conditions for positive, negative, and neutral
-            positive_condition_o_tone = df['o_tone'] > 70
-            positive_condition_r_tone = df['r_tone'] > 70
-            negative_condition_o_tone = df['o_tone'] < 30
-            negative_condition_r_tone = df['r_tone'] < 30
+            positive_condition_o_tone = df['o_tone'] > upper_bound
+            positive_condition_r_tone = df['r_tone'] > upper_bound
+            negative_condition_o_tone = df['o_tone'] < lower_bound
+            negative_condition_r_tone = df['r_tone'] < lower_bound
 
             # Count occurrences where 'o_tone' is negative or neutral while 'r_tone' is positive
             count_negative_or_neutral_o_tone_positive_r_tone = (negative_condition_o_tone | (df['o_tone'] == 0)) & positive_condition_r_tone
@@ -76,7 +76,7 @@ with open(output_file, 'a') as f:
             print(f"Count where 'r_tone' is negative or neutral while 'o_tone' is positive: {count_negative_or_neutral_r_tone_positive_o_tone}")
 
             # Define conditions for positive, negative, and neutral
-            positive_condition = df['o_tone'] > 70
+            positive_condition = df['o_tone'] > upper_bound
             negative_condition_i_tone = (df['i_tone'] < 0) & (df['o_tone'] > 0)
             negative_condition_r_tone = (df['i_tone'] < 0) & (df['r_tone'] > 0)
 
@@ -84,7 +84,7 @@ with open(output_file, 'a') as f:
             true_labels = [1 if pos else (-1 if neg_i else 0) for pos, neg_i in zip(positive_condition, negative_condition_i_tone)]
 
             # Assign predicted labels based on 'r_tone' predictions
-            predicted_labels = [1 if val > 70 else (-1 if val < 30 else 0) for val in df['r_tone']]
+            predicted_labels = [1 if val > upper_bound else (-1 if val < lower_bound else 0) for val in df['r_tone']]
 
             # Calculate F1 score for the current file
             f1 = f1_score(true_labels, predicted_labels, average='weighted') * 100
