62533f488e653cd168a9d7cb3387ab5ebc6f25e5
Yerong <yerong.li@outlook.com>
2023-12-15 10:10:41 -0600

    his_len

--- length.py
+++ length.py
@@ -108,7 +108,7 @@ with open(output_file, 'a') as f:
 
             # Process r_tone_ratios
             r_tone_ratios['end'] = [0.0, 0.25, 0.75]
-            r_tone_ratios['begin'] = [0.0379746835443038, 0.6329113924050633, 0.3291139240506329]
+            r_tone_ratios['begin'] = [0.0179746835443038, 0.6329113924050633, 0.3291139240506329]
             raw_data_r = process(r_tone_ratios)
             df_r = pd.DataFrame(raw_data_r)
             r = [0,1,2]
