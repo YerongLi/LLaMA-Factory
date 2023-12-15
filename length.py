008413101017fa9fd2db093ea8e82e3b7d9514a9
Yerong <yerong.li@outlook.com>
2023-12-15 10:08:09 -0600

    his_len

--- length.py
+++ length.py
@@ -107,6 +107,7 @@ with open(output_file, 'a') as f:
             df_o = pd.DataFrame(raw_data_o)
 
             # Process r_tone_ratios
+            print(r_tone_ratios)
             raw_data_r = process(r_tone_ratios)
             df_r = pd.DataFrame(raw_data_r)
             r = [0,1,2]
