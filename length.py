f855a896f94e24eb1941de5f232c19fd34cbe2d9
Yerong <yerong.li@outlook.com>
2023-12-15 09:05:06 -0600

    his_len

--- length.py
+++ length.py
@@ -56,5 +56,44 @@ with open(output_file, 'a') as f:
             length = df['his_len']
             df['current_length'] = df['history'].apply(lambda x: len(eval(x)))
             df['ratio'] = df['current_length'] / df['his_len']
-            print(df['ratio'])
+            # print(df['ratio'])
+            # Filter df for each ratio range
+            df_less_than_third = df[df['ratio'] < 1/3]
+            df_between_third_and_two_thirds = df[(df['ratio'] >= 1/3) & (df['ratio'] < 2/3)]
+            df_greater_than_two_thirds = df[df['ratio'] >= 2/3]
 
+            # Function to calculate ratio of -1, 0, 1 for a specific tone column
+            def calculate_ratio_for_tone(df_subset, tone_column):
+                total_count = len(df_subset)
+                negative_count = (df_subset[tone_column] == -1).sum()
+                neutral_count = (df_subset[tone_column] == 0).sum()
+                positive_count = (df_subset[tone_column] == 1).sum()
+
+                ratio_negative = negative_count / total_count
+                ratio_neutral = neutral_count / total_count
+                ratio_positive = positive_count / total_count
+
+                return ratio_negative, ratio_neutral, ratio_positive
+
+            # Calculate ratios for o_tone_mapped in each ratio range
+            ratio_negative_o_less_than_third, ratio_neutral_o_less_than_third, ratio_positive_o_less_than_third = calculate_ratio_for_tone(df_less_than_third, 'o_tone_mapped')
+            ratio_negative_o_between_third_and_two_thirds, ratio_neutral_o_between_third_and_two_thirds, ratio_positive_o_between_third_and_two_thirds = calculate_ratio_for_tone(df_between_third_and_two_thirds, 'o_tone_mapped')
+            ratio_negative_o_greater_than_two_thirds, ratio_neutral_o_greater_than_two_thirds, ratio_positive_o_greater_than_two_thirds = calculate_ratio_for_tone(df_greater_than_two_thirds, 'o_tone_mapped')
+
+            # Calculate ratios for r_tone_mapped in each ratio range
+            ratio_negative_r_less_than_third, ratio_neutral_r_less_than_third, ratio_positive_r_less_than_third = calculate_ratio_for_tone(df_less_than_third, 'r_tone_mapped')
+            ratio_negative_r_between_third_and_two_thirds, ratio_neutral_r_between_third_and_two_thirds, ratio_positive_r_between_third_and_two_thirds = calculate_ratio_for_tone(df_between_third_and_two_thirds, 'r_tone_mapped')
+            ratio_negative_r_greater_than_two_thirds, ratio_neutral_r_greater_than_two_thirds, ratio_positive_r_greater_than_two_thirds = calculate_ratio_for_tone(df_greater_than_two_thirds, 'r_tone_mapped')
+
+            # Print the results
+            print("For ratio < 1/3:")
+            print(f"o_tone_mapped ratios: -1:{ratio_negative_o_less_than_third:.2%}, 0:{ratio_neutral_o_less_than_third:.2%}, 1:{ratio_positive_o_less_than_third:.2%}")
+            print(f"r_tone_mapped ratios: -1:{ratio_negative_r_less_than_third:.2%}, 0:{ratio_neutral_r_less_than_third:.2%}, 1:{ratio_positive_r_less_than_third:.2%}\n")
+
+            print("For 1/3 <= ratio < 2/3:")
+            print(f"o_tone_mapped ratios: -1:{ratio_negative_o_between_third_and_two_thirds:.2%}, 0:{ratio_neutral_o_between_third_and_two_thirds:.2%}, 1:{ratio_positive_o_between_third_and_two_thirds:.2%}")
+            print(f"r_tone_mapped ratios: -1:{ratio_negative_r_between_third_and_two_thirds:.2%}, 0:{ratio_neutral_r_between_third_and_two_thirds:.2%}, 1:{ratio_positive_r_between_third_and_two_thirds:.2%}\n")
+
+            print("For ratio >= 2/3:")
+            print(f"o_tone_mapped ratios: -1:{ratio_negative_o_greater_than_two_thirds:.2%}, 0:{ratio_neutral_o_greater_than_two_thirds:.2%}, 1:{ratio_positive_o_greater_than_two_thirds:.2%}")
+            print(f"r_tone_mapped ratios: -1:{ratio_negative_r_greater_than_two_thirds:.2%}, 0:{ratio_neutral_r_greater_than_two_thirds:.2%}, 1:{ratio_positive_r_greater_than_two_thirds:.2%}")
