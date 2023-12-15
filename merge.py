bfb94cc7d0657edbcfed6c81816eb48c1135aa45
Yerong <yerong.li@outlook.com>
2023-12-15 08:54:56 -0600

    his_len

--- merge.py
+++ merge.py
@@ -34,7 +34,10 @@ def process_filenames(folder_path):
                     df[f"prompt"] = file_df['prompt']
                     df[f"history"] = file_df['history']
                     df[f"summary"] = file_df['summary']
-                    df[f"his_len"] = file_df['his_len']
+                    try:
+                        df[f"his_len"] = file_df['his_len']
+                    except:
+                        pass
 
                 # Save tone with a prefix in the column name
                 df[f"{prefix}_tone"] = file_df['Tone']
