a90ec4a3c6164edfb407cf2114eb377ce2e2e38e
Yerong <yerong.li@outlook.com>
2023-12-15 09:56:23 -0600

    his_len

--- length.py
+++ length.py
@@ -100,8 +100,8 @@ with open(output_file, 'a') as f:
             names = ['begin','middle', 'end']
             def process(tone_ratios):
                 raw_data = {'greenBars': [[tone_ratios[name][0]] for name in names]}
-                raw_data = {'orangeBars': [[tone_ratios[name][1]] for name in names]}
-                raw_data = {'blueBars': [[tone_ratios[name][2]] for name in names]}
+                raw_data .update({'orangeBars': [[tone_ratios[name][1]] for name in names]})
+                raw_data .update({'blueBars': [[tone_ratios[name][2]] for name in names]})
                 return raw_data
             raw_data = process(o_tone_ratios)
             df = pd.DataFrame(raw_data)
