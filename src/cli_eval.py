7c2896ccbbe3d680919956f6b90d776a20fddb9b
Yerong <yerong.li@outlook.com>
2023-12-19 12:15:24 -0600

    Print the number of events

--- src/cli_eval.py
+++ src/cli_eval.py
@@ -72,10 +72,10 @@ def main():
 
     ans = {}
 
-    # random.shuffle(data)
-    # for record in tqdm.tqdm(data[:10]):
+    random.shuffle(data)
+    for record in tqdm.tqdm(data[:10]):
 
-    for record in tqdm.tqdm(data):
+    # for record in tqdm.tqdm(data):
         instruction = record["instruction"]
         # logging.info('Summary')
         # logging.info(record["summary"])
@@ -106,7 +106,7 @@ def main():
             'summary': summary,
             'his_len': record["his_len"],
         }
-
+        print(record_type)
         # Append the pair to the corresponding record type list in the dictionary
         if record_type not in ans:
             ans[record_type] = []
