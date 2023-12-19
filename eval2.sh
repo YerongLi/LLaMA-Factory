2cf07c02cd66810ba112eb237b0510c8544c194e
Yerong <yerong.li@outlook.com>
2023-12-19 10:23:40 -0600

    Upload new script

---
+++ eval2.sh
@@ -0,0 +1,5 @@
+python src/cli_eval.py \
+    --model_name_or_path /scratch/yerong/.cache/pyllama/Llama-2-7b-hf \
+    --template dispatcher \
+    --finetuning_type lora \
+    --checkpoint_dir police1/checkpoint-4410
