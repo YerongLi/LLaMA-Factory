1953ad3a47cfa8984f09006dda696cc665bb2b4e
Yerong <yerong.li@outlook.com>
2023-12-19 06:01:26 -0600

    Train quantized model

--- src/cli_eval2.py
+++ src/cli_eval2.py
@@ -99,6 +99,7 @@ def main():
 
             batch_prompt_ids.append(prompt_ids)
         print(batch_prompt_ids)
+        output_sequences = model.generate(**batch_prompt_ids, max_new_tokens=20, do_sample=True, top_p=0.9)
 
         # Create a dictionary with the response and output pair
         response_output_pair = {
