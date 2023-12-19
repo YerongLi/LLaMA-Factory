11463a14c9bf42d6a50b5a4c1be59bbf15b702e0
Yerong <yerong.li@outlook.com>
2023-12-19 05:00:09 -0600

    Upload

---
+++ eval4.sh
@@ -0,0 +1,6 @@
+python src/cli_eval.py \
+    --model_name_or_path /scratch/yerong/.cache/pyllama/Llama-2-7b-hf \
+    --template dispatcher \
+    --finetuning_type lora \
+    --quantization_bit 4 \
+    --checkpoint_dir police4/best
