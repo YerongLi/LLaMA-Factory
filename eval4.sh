8646f3c204deb8e332de48cfad31464706fd9fc1
Yerong <yerong.li@outlook.com>
2023-12-22 07:13:36 -0600

    Remove continue

--- eval4.sh
+++ eval4.sh
@@ -1,4 +1,4 @@
-python src/cli_eval.py \
+python src/cli_eval3.py \
     --model_name_or_path /scratch/yerong/.cache/pyllama/Llama-2-7b-hf \
     --template dispatcher \
     --finetuning_type lora \
