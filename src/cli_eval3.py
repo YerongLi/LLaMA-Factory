6d062ea859d53ce88e9c9e3c948ee76a8731f8df
Yerong <yerong.li@outlook.com>
2023-12-22 14:46:19 -0600

    Upload the Trainer evaluation

--- src/cli_eval3.py
+++ src/cli_eval3.py
@@ -37,9 +37,9 @@ try:
 except ImportError:
     print("Install `readline` for a better experience.")
 model_name = '/scratch/yerong/.cache/pyllama/Llama-2-7b-hf'
-tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
-tokenizer.pad_token = "[PAD]"
-tokenizer.padding_side = "left"
+# tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
+# tokenizer.pad_token = "[PAD]"
+# tokenizer.padding_side = "left"
 
 def main():
     chat_model = ChatModel()
