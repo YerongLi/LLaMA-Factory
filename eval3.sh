09785ce33ce701e235493b42fdf357050d43f5b2
Yerong <yerong.li@outlook.com>
2023-12-22 04:16:01 -0600

    Upload the Trainer evaluation

--- eval3.sh
+++ eval3.sh
@@ -1,1 +1,5 @@
-python src/cli_eval3.py
+python src/cli_eval3.py \
+    --model_name_or_path /scratch/yerong/.cache/pyllama/Llama-2-7b-hf \
+    --template dispatcher \
+    --finetuning_type lora \
+    --checkpoint_dir police1/checkpoint-4410
