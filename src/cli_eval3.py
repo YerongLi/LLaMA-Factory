d44c785be48991267d765e6b684993f386474f66
Yerong <yerong.li@outlook.com>
2023-12-22 07:17:26 -0600

    Remove continue

--- src/cli_eval3.py
+++ src/cli_eval3.py
@@ -22,7 +22,7 @@ if os.path.exists(LOGFILE):
 else:
     print(f"The file {LOGFILE} does not exist.")
 rouge = Rouge()
-BATCH_SIZE = 8
+BATCH_SIZE = 32
 logging.basicConfig(
     format='%(asctime)s %(levelname)-4s - %(filename)-6s:%(lineno)d - %(message)s',
     level=logging.INFO,
