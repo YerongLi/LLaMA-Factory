86c54686b277ef5f6c36d3f83d88baf8525f643f
Yerong <yerong.li@outlook.com>
2023-12-22 16:26:55 -0600

    Print per_device_eval_batch_size

--- src/cli_eval3.py
+++ src/cli_eval3.py
@@ -44,7 +44,8 @@ def main():
     with open("data/police1.json", "r") as file:
         data = [json.loads(line) for line in file]
     chat_model = ChatModel()
-    print(chat_model.finetuning_args.per_device_eval_batch_size)
+    print(chat_model.generating_args)
+    print(chat_model.generating_args.per_device_eval_batch_size)
     # Initialize BLEURT
     # bleurt_scorer = bleurt.score.BleurtScorer("bleurt-base-128")
 
