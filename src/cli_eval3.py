0a3342b8d02206875558c4c93f72d639b2554cc5
Yerong <yerong.li@outlook.com>
2023-12-22 07:22:09 -0600

    Remove continue

--- src/cli_eval3.py
+++ src/cli_eval3.py
@@ -140,6 +140,11 @@ def main():
         # print(tokenized_prompts)
         try:
             generated_outputs = chat_model.model.generate(**tokenized_prompts, max_new_tokens=20, do_sample=True, top_p=0.9)
+            for prompt, generated_output in zip(tokenized_prompts["input_ids"], generated_outputs):
+                decoded_output = tokenizer.decode(generated_output, skip_special_tokens=True)
+                print(f"Input: {tokenizer.decode(prompt, skip_special_tokens=True)}")
+                print(f"Generated Output: {decoded_output}")
+                print("=" * 50)
         except KeyboardInterrupt:
             break
         except:
