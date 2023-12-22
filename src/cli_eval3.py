cb13bbc8ec394b1c23108b4794ca33255785168d
Yerong <yerong.li@outlook.com>
2023-12-22 07:23:55 -0600

    Remove continue

--- src/cli_eval3.py
+++ src/cli_eval3.py
@@ -139,6 +139,7 @@ def main():
     for tokenized_prompts in tqdm(tokenized_prompt_batches):
         # print(tokenized_prompts)
         try:
+            print(tokenized_prompts)
             generated_outputs = chat_model.model.generate(**tokenized_prompts, max_new_tokens=20, do_sample=True, top_p=0.9)
             for prompt, generated_output in zip(tokenized_prompts["input_ids"], generated_outputs):
                 decoded_output = tokenizer.decode(generated_output, skip_special_tokens=True)
