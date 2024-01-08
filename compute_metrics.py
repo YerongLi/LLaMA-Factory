8b6b9ddf255ad6cc3f151b06883675995b7e0196
Yerong Li <yerong.li@outlook.com>
2024-01-08 00:34:17 -0500

    Evaluate on data/police1.json

--- compute_metrics.py
+++ compute_metrics.py
@@ -41,7 +41,8 @@ def main():
     # print("Welcome to the CLI application, use `clear` to remove the history, use `exit` to exit the application.")
 
     # Load data from the file
-    with open("results.jsonl", "r") as file:
+    with open("data/police1.json", "r") as file:
+    # with open("results.jsonl", "r") as file:
         data = [json.loads(line) for line in file]
     # Initialize BLEURT
     # bleurt_scorer = bleurt.score.BleurtScorer("bleurt-base-128")
