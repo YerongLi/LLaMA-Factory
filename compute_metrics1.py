bf2c3c57699fdf2c579879d7ad0020ce534b217e
Yerong Li <yerong.li@outlook.com>
2024-01-15 14:20:51 -0500

    Upload

--- compute_metrics1.py
+++ compute_metrics1.py
@@ -12,8 +12,9 @@ dataset_dict = datasets.load_dataset(dataset_name, dataset_config_name)
 dataset_dict[split_name][0]
 
 classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+print(dataset_dict[split_name]["text"][0])
 
 model_outputs = classifier(dataset_dict[split_name]["text"]) 
 
-print(dataset_dict[split_name]["text"][0])
+# print(dataset_dict[split_name]["text"][0])
 print(model_outputs[0])
