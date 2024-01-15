f84db514c0e8a2721276e6f49d88574c59f8413a
Yerong Li <yerong.li@outlook.com>
2024-01-15 16:55:35 -0500

    Upload compute metric1

--- compute_metrics2.py
+++ compute_metrics2.py
@@ -34,8 +34,8 @@ def HASH(input_string):
     hash_value = int.from_bytes(hash_object.digest(), byteorder='big')
 
     return str(hash_value)
-file_name = "results_gpt35.jsonl"
-# file_name = "results.jsonl"
+# file_name = "results_gpt35.jsonl"
+file_name = "results.jsonl"
 # file_name = "results-bak.jsonl"
 
 random_seed = int(HASH(file_name))
