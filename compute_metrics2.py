1806c2b3847b3915d76c1d11a24dba9dd50a3c04
Yerong Li <yerong.li@outlook.com>
2024-01-15 15:29:32 -0500

    tqdm show progress

--- compute_metrics2.py
+++ compute_metrics2.py
@@ -13,8 +13,17 @@ gpu_index = num_gpus - 1 if num_gpus > 0 else -1
 device_str = f'cuda:{gpu_index}' if gpu_index >= 0 else 'cpu'
 tokenizer = AutoTokenizer.from_pretrained("SchuylerH/bert-multilingual-go-emtions")
 model = AutoModelForSequenceClassification.from_pretrained("SchuylerH/bert-multilingual-go-emtions").to(device_str)
-print(model.config.label2id)
+
 batch_size = 16  # Set your desired batch size
+
+# Mapping of emotions to labels
+emotion_mapping = {
+    'admiration': 1, 'amusement': 1, 'approval': 1, 'caring': 1, 'desire': 1, 'excitement': 1,
+    'gratitude': 1, 'joy': 1, 'love': 1, 'optimism': 1, 'pride': 1, 'relief': 1,
+    'anger': -1, 'annoyance': -1, 'disappointment': -1, 'disapproval': -1,
+    'disgust': -1, 'embarrassment': -1, 'fear': -1, 'grief': -1, 'nervousness': -1,
+    'remorse': -1, 'sadness': -1
+}
 
 with open("results_gpt35.jsonl", "r") as file:
     data = [json.loads(line) for line in file]
@@ -32,6 +41,9 @@ for i in tqdm(range(0, len(data), batch_size)):
     # Get the predicted labels for the batch
     batch_predicted_labels = batch_outputs.logits.argmax(dim=1).tolist()
 
-    # Print the predicted labels for the batch
-    # for j, predicted_label in enumerate(batch_predicted_labels):
-        # print(f"Prediction for data[{i + j}]: {predicted_label}")
+    # Map predicted labels to desired values
+    mapped_labels = [emotion_mapping[model.config.id2label[label]] for label in batch_predicted_labels]
+
+    # Print the mapped labels for the batch
+    for j, mapped_label in enumerate(mapped_labels):
+        print(f"Mapping for data[{i + j}]: {mapped_label}")
