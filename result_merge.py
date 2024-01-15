149a0cb69b6fda7b60ca4ed629555c62f07df089
Yerong Li <yerong.li@outlook.com>
2024-01-15 18:00:41 -0500

    Upload merge_result

---
+++ result_merge.py
@@ -0,0 +1,23 @@
+import json
+import random
+
+# Set a fixed random seed for reproducibility
+random_seed = 42
+random.seed(random_seed)
+
+with open("results-cmp.jsonl", "r") as file:
+    data1 = [json.loads(line) for line in file]
+
+with open("results.jsonl", "r") as file:
+    data = [json.loads(line) for line in file]
+
+# Randomly copy response with a probability of 0.1
+for i in range(len(data)):
+    if random.random() < 0.1 and i < len(data1):
+        data[i]['response'] = data1[i]['response']
+
+# Save the modified data to results1.jsonl
+with open("results1.jsonl", "w") as file:
+    for record in data:
+        json.dump(record, file)
+        file.write("\n")
