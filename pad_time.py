87ce381bf6266649bc3926ee5afe9bd8fd92f572
Yerong Li <yerong.li@outlook.com>
2024-01-16 00:46:24 -0500

    Upload merge_result

--- pad_time.py
+++ pad_time.py
@@ -49,8 +49,9 @@ with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
             row['o_tone'] = str(int(num*100)/100)
         # if row['o_tone'] and random_number % 8 == 2 and float(row['o_tone']) > 60: row['o_tone'] = str(float(15 + random.randint(1, 10)))
 
-        if row['o_tone'] and  random_number % 8 == 0 and int(row['hour']) > 21 or int(row['hour']) < 3 and float(row['o_tone']) > 60 : row['o_tone'] = str(float(40 + int(round(random.uniform(1, 10)*100))/100))
-
+        if row['o_tone'] and  random_number % 8 == 0 and int(row['hour']) > 21 or int(row['hour']) < 3 and float(row['o_tone']) > 60 : 
+            num = 40 + random.uniform(1, 10)
+            row['o_tone'] = str(int(num*100)/100)
         # if row['o_tone'] and  random_number % 6 == 1 and int(row['hour']) > 6 or int(row['hour']) < 12 and float(row['o_tone']) < 60 : row['o_tone'] = str(float(80 + random.randint(1, 10)))
         print(row['o_tone'])
         new_csv_rows.append(row)
