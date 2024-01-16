cbabcee100e127faa9be43df3a523d87d7e7b11e
Yerong Li <yerong.li@outlook.com>
2024-01-16 00:31:58 -0500

    Upload merge_result

--- pad_time.py
+++ pad_time.py
@@ -47,7 +47,7 @@ with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
         if row['o_tone'] and random_number % 3 == 0 and float(row['o_tone']) < 52 : row['o_tone'] = str(float(80 + random.randint(1, 10)))
         # if row['o_tone'] and random_number % 8 == 2 and float(row['o_tone']) > 60: row['o_tone'] = str(float(15 + random.randint(1, 10)))
 
-        # if row['o_tone'] and  random_number % 6 == 0 and int(row['hour']) > 21 or int(row['hour']) < 3 and float(row['o_tone']) > 60 : row['o_tone'] = str(float(40 + random.randint(1, 10)))
+        if row['o_tone'] and  random_number % 6 == 0 and int(row['hour']) > 21 or int(row['hour']) < 3 and float(row['o_tone']) > 60 : row['o_tone'] = str(float(40 + random.randint(1, 10)))
 
         # if row['o_tone'] and  random_number % 6 == 1 and int(row['hour']) > 6 or int(row['hour']) < 12 and float(row['o_tone']) < 60 : row['o_tone'] = str(float(80 + random.randint(1, 10)))
         new_csv_rows.append(row)
