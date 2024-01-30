a451f4a3dd89643136a160508d2b6acb4256ca6f
Yerong Li <yerong.li@outlook.com>
2024-01-30 16:38:06 -0500

    LLama

--- count2.py
+++ count2.py
@@ -1,8 +1,8 @@
 import json
 
 # file_name = "results_gpt35.jsonl"
-# file_name = "results.jsonl"
-file_name = "results-bak.jsonl"
+file_name = "results.jsonl"
+# file_name = "results-bak.jsonl"
 
 # Event types
 event_types = ['SuspiciousActivity', 'AccidentTrafficParking', 'DrugsAlcohol', 'EmergencyMessage', 'FacilitiesMaintenance', 'HarassmentAbuse', 'MentalHealth', 'NoiseDisturbance', 'TheftLostItem']
