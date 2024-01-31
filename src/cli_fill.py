78032763e04811eeba0f69ba885d5471b2812b47
Yerong Li <yerong.li@outlook.com>
2024-01-31 17:11:25 -0500

    Use phi-1_5

--- src/cli_fill.py
+++ src/cli_fill.py
@@ -125,6 +125,7 @@ def main():
     # Iterate through each batch of data
     prompt_batches = []
     failed_count = 0
+    print(type(chat_model.template))
     for batch in tqdm(data_batches):
         # Iterate through each record in the batch
         prompt_batch = []
@@ -140,7 +141,7 @@ def main():
             
                 output = record["output"]
 
-                prompt_ids, _ = chat_model.template.encode_oneturn(
+                prompt_ids, _ = chat_model.encode_oneturn(
                     tokenizer=chat_model.tokenizer, query=instruction, resp="", history=history, system=chat_model.template.system+f'\n{summary}'
                 )
                 prompt = chat_model.tokenizer.decode(
