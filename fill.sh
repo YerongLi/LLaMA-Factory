5bea8734951aa95bc8e8aad972954679d31eaa9d
Yerong Li <yerong.li@outlook.com>
2024-01-31 14:57:29 -0500

    Fill in

---
+++ fill.sh
@@ -0,0 +1,6 @@
+python src/cli_fill.py \
+    --model_name_or_path /scratch/yerong/.cache/pyllama/phi-2 \
+    --template dispatcher \
+    --finetuning_type lora \
+    --quantization_bit 4 \
+    --checkpoint_dir bakpolice4/best
