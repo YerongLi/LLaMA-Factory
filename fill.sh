80b9d4e4f204b4e332f4e349ac700a94f1bbf5a5
Yerong Li <yerong.li@outlook.com>
2024-01-31 15:26:31 -0500

    Use phi-1_5

--- fill.sh
+++ fill.sh
@@ -1,6 +1,4 @@
 python src/cli_fill.py \
     --model_name_or_path /scratch/yerong/.cache/pyllama/phi-1_5 \
     --template dispatcher \
-    --finetuning_type lora \
-    --quantization_bit 4 \
-    --checkpoint_dir bakpolice4/best
+    --quantization_bit 4
