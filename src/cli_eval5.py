6fdaebed345cfe5b872be3823b3ae96eb7cf92b9
Yerong Li <yerong.li@outlook.com>
2024-02-09 03:03:36 -0500

    batch size 16

--- src/cli_eval5.py
+++ src/cli_eval5.py
@@ -148,6 +148,7 @@ def main():
                  {record['output']}
                  Revised Dispatcher's response:
                  """
+                logging.info(prompt)
                 # print(prompt)
                 # print('=====  ===== ========')
                 # print('response' in record)
