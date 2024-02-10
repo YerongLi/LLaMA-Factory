38ba8fc2f3d21a9605a44e9bbfe6eb3422e133bb
Yerong Li <yerong.li@outlook.com>
2024-02-10 05:10:41 -0500

    Upload

--- src/cli_eval3.py
+++ src/cli_eval3.py
@@ -45,7 +45,8 @@ def main():
     import json
     text_with_newline = "\n"
 
-    with open("data/police1.json", "r") as file:
+    # with open("data/police1.json", "r") as file:
+    with open("data/police2.json", "r") as file:
         data = [json.loads(line) for line in file]
     chat_model = ChatModel()
     tokens = chat_model.tokenizer.encode(text_with_newline)
