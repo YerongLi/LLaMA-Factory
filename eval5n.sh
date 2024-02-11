e5ebe7878159c9aa0e9bd5189f4dfc69cacdee01
Yerong Li <yerong.li@outlook.com>
2024-02-11 07:38:02 -0500

    Test flatten dataset

--- eval5n.sh
+++ eval5n.sh
@@ -2,5 +2,4 @@ python src/cli_eval5.py \
     --model_name_or_path /scratch/bbrz/yirenl2/models/Llama-2-7b-hf \
     --template dispatcher \
     --finetuning_type lora \
-    --quantization_bit 4 \
-    --checkpoint_dir bakpolice4/best
+    --quantization_bit 4
