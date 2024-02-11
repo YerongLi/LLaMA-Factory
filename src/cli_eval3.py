d98cac62a145bf786e04866f91bce256eac0f22a
Yerong Li <yerong.li@outlook.com>
2024-02-11 04:47:55 -0500

    Reformat the query and response

--- src/cli_eval3.py
+++ src/cli_eval3.py
@@ -124,12 +124,12 @@ def main():
                 record_type = record.get('type', 'unknown').replace('/', '').replace(' ', '')
                 summary = record["summary"] if 'summary' in record else ''
 
-                # response = chat_model.chat(query=instruction, history=history, system=chat_model.template.system+f'\n{summary}')[0].response_text
+                response = chat_model.chat(query=instruction, history=history[:-1], system=chat_model.template.system+f'\n{summary}')[0].response_text
             
                 output = record["output"]
 
                 prompt_ids, _ = chat_model.template.encode_oneturn(
-                    tokenizer=chat_model.tokenizer, query=instruction, resp="", history=history, system=chat_model.template.system+f'\n{summary}'
+                    tokenizer=chat_model.tokenizer, query=instruction, resp=None, history=history, system=chat_model.template.system+f'\n{summary}'
                 )
                 prompt = chat_model.tokenizer.decode(
                     prompt_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
