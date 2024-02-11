23c6068786a1c4b7bac48d865691214d31f27b81
Yerong Li <yerong.li@outlook.com>
2024-02-11 05:34:10 -0500

    Reformat the query and response

--- src/llmtuner/data/template.py
+++ src/llmtuner/data/template.py
@@ -44,12 +44,13 @@ class Template:
         prompt_ids = []
         for query_ids, resp_ids in encoded_pairs[:-1]:
             logging.info('encoded')
+            prompt = tokenizer.decode(
+                            query_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )
+            logging.info(prompt)
             prompt_ids = prompt_ids + query_ids + resp_ids
         prompt_ids, answer_ids = prompt_ids + encoded_pairs[-1][0], encoded_pairs[-1][1]
-        prompt = tokenizer.decode(
-                            prompt_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
-        )
-        logging.info(prompt)
+
         return prompt_ids, answer_ids
 
     def encode_multiturn(
