11ca599920487049575a3c3329f6a1b587e963dd
Yerong Li <yerong.li@outlook.com>
2024-02-11 06:07:24 -0500

    Reformat the query and response

--- src/llmtuner/data/template.py
+++ src/llmtuner/data/template.py
@@ -46,6 +46,7 @@ class Template:
         for query_ids, resp_ids in encoded_pairs[:-1]:
             prompt_ids = prompt_ids + query_ids + resp_ids
         # prompt_ids, answer_ids = prompt_ids + encoded_pairs[-1][0], encoded_pairs[-1][1]
+        logging.info([tokenizer.convert_tokens_to_ids(f'{self.target}: ')])
         prompt_ids = prompt_ids + [tokenizer.convert_tokens_to_ids(f'{self.target}: ')]
         answer_ids = encoded_pairs[-1][0]
 
