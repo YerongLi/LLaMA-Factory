7bae66800737404cb5025f2d32ba70419ad3cde5
Yerong Li <yerong.li@outlook.com>
2024-02-11 20:35:48 -0500

    Add emo-police dataset

--- src/llmtuner/data/loader.py
+++ src/llmtuner/data/loader.py
@@ -74,7 +74,7 @@ def get_dataset(
         for entry in dataset_list:
             logging.info(entry)
         for entry in dataset:
-            logging.info(entry)
+            logging.info(type(entry))
         if data_args.streaming and (dataset_attr.load_from == "file"):
             dataset = dataset.to_iterable_dataset() # TODO: add num shards parameter
 
