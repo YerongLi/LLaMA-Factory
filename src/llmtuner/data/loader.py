9218bf3ee95a5505f5c1a2423d6de9097dc061f6
Yerong Li <yerong.li@outlook.com>
2024-02-11 20:16:40 -0500

    Add emo-police dataset

--- src/llmtuner/data/loader.py
+++ src/llmtuner/data/loader.py
@@ -55,6 +55,7 @@ def get_dataset(
             raise NotImplementedError
         logging.info(data_files)
         if data_path == 'json':
+            import json
             with open(data_files[0], "r") as file:
                 dataset = [json.loads(line) for line in file]
         else :
