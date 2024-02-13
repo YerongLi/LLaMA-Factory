e865ec3d42c4f4e37a88aa7f5c09245c3d7f9ff5
Yerong Li <yerong.li@outlook.com>
2024-02-13 18:32:26 -0500

    Add emotion

--- eval4.sh
+++ eval4.sh
@@ -3,4 +3,4 @@ python src/cli_eval3.py \
     --template dispatcher \
     --finetuning_type lora \
     --quantization_bit 4 \
-    --checkpoint_dir bakpolice4/best
+    --checkpoint_dir police4/checkpoint-2870
