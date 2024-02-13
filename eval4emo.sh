b459a9a54141c50ca21deaf760f179836c8dfb2d
Yerong Li <yerong2@gpua019.delta.ncsa.illinois.edu>
2024-02-13 11:11:45 -0600

    Upload

---
+++ eval4emo.sh
@@ -0,0 +1,6 @@
+python src/cli_eval3.py \
+    --model_name_or_path /scratch/yerong/.cache/pyllama/Llama-2-7b-chat-hf \
+    --template dispatcher \
+    --finetuning_type lora \
+    --quantization_bit 4 \
+    --checkpoint_dir /scratch/yerong/Factory/emopolice4/checkpoint-1390 
---
+++ eval4emon.sh
@@ -0,0 +1,6 @@
+python src/cli_eval3.py \
+    --model_name_or_path /scratch/bbrz/yirenl2/models/Llama-2-7b-chat-hf \
+    --template dispatcher \
+    --finetuning_type lora \
+    --quantization_bit 4 \
+    --checkpoint_dir emopolice4/checkpoint-1510 
