a4d9848f8202c0cf4874be22c317d9968fd6a08c
Yerong Li <yerong.li@outlook.com>
2024-02-13 14:51:12 -0500

    concatenate prompt

--- src/cli_fill.py
+++ src/cli_fill.py
@@ -185,7 +185,7 @@ Dialogue 2:
                 for match in matches:
                     unique_texts.add(match)
                 prompt_ids, target_ids = chat_model.template.encode_oneturn(
-                    tokenizer=chat_model.tokenizer, query=instruction, resp="", history=history, system=chat_model.template.system+f'\n{summary}'
+                    tokenizer=chat_model.tokenizer, query=None, resp="", history=history, system=chat_model.template.system+f'\n{summary}'
                 )
                 prompt_ids += target_ids
                 prompt = chat_model.tokenizer.decode(
