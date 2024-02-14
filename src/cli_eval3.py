402ff3fbe809d2417b83103cde1e7d65a50c4828
Yerong Li <yerong.li@outlook.com>
2024-02-13 20:44:39 -0500

    Upload

--- src/cli_eval3.py
+++ src/cli_eval3.py
@@ -114,6 +114,8 @@ def main():
 
     data_batches = [data_empty[i:i + BATCH_SIZE] for i in range(0, len(data_empty), BATCH_SIZE)]+[data_fill]
     print('data_empty', len(data_empty))
+    print('data_fill', len(data_fill))
+    
     # for record in tqdm.tqdm(data[:60]):
     # Iterate through each batch of data
     prompt_batches = []
