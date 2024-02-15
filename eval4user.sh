ce2e5213d80068f4dd583edb8707c620bf9fff38
Yerong Li <yerong.li@outlook.com>
2024-02-15 13:19:23 -0500

    Generate script for user

---
+++ eval4user.sh
@@ -0,0 +1,7 @@
+python src/cli_eval3.py \
+    --model_name_or_path $CHATLM \
+    --template user \
+    --dataset usertest \
+    --finetuning_type lora \
+    --quantization_bit 4 \
+    --checkpoint_dir user4/best
