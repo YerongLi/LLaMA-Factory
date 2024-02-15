4c998b938996e2417c2c33f75e68ca68df111978
Yerong Li <yerong.li@outlook.com>
2024-02-15 13:20:28 -0500

    Generate script for user

--- eval4user.sh
+++ eval4user.sh
@@ -4,4 +4,4 @@ python src/cli_eval3.py \
     --dataset usertest \
     --finetuning_type lora \
     --quantization_bit 4 \
-    --checkpoint_dir user4/best
+    --checkpoint_dir user4/checkpoint-1840/
