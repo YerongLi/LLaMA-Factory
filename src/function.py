376a149ae7cc80356f5ca583fc41622ce40f6b9a
Yerong Li <yerong.li@outlook.com>
2024-02-19 13:02:42 -0500

    function.sh

--- src/function.py
+++ src/function.py
@@ -6,7 +6,7 @@ import random
 from tqdm import tqdm
 import csv
 import traceback
-
+from src.llmtuner.data import Template
 
 from llmtuner import ChatModel
 from llmtuner.extras.misc import torch_gc
