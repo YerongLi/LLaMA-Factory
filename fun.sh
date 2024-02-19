ce46fa6bcd36165f518585a7b5bc07a69a7e851f
Yerong Li <yerong.li@outlook.com>
2024-02-19 12:22:54 -0500

    function.sh

--- fun.sh
+++ fun.sh
@@ -1,5 +1,6 @@
-python src/function.py \
+python src/cli_eval3.py \
     --model_name_or_path $CHATLM \
     --template dispatcher \
     --finetuning_type lora \
+    --quantization_bit 4 \
     --checkpoint_dir police4/best
