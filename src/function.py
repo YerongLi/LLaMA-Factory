e759e8f04d6f8aa2f31d0b955e2b0463fd953d3d
Yerong Li <yerong.li@outlook.com>
2024-02-19 13:11:56 -0500

    function.sh

--- src/function.py
+++ src/function.py
@@ -6,8 +6,7 @@ import random
 from tqdm import tqdm
 import csv
 import traceback
-from src.llmtuner.data import Template
-
+from llmtuner.data.template import get_template_and_fix_tokenizer
 from llmtuner import ChatModel
 from llmtuner.extras.misc import torch_gc
 from transformers import AutoTokenizer, AutoModelForCausalLM
