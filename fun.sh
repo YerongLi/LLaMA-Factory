e1480c240178717e74db9c4e22304e37e5368f4f
Yerong Li <yerong.li@outlook.com>
2024-02-19 12:35:04 -0500

    Upload

--- fun.sh
+++ fun.sh
@@ -1,6 +1,5 @@
-python src/cli_eval3.py \
+python src/cli_eval5.py \
     --model_name_or_path $LM \
     --template dispatcher \
     --finetuning_type lora \
-    --quantization_bit 4 \
-    --checkpoint_dir police4/best
+    --quantization_bit 4
