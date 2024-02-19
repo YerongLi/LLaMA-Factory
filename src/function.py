984ac404cde92389395e11fbc919c39b7ddfcc87
Yerong Li <yerong.li@outlook.com>
2024-02-19 14:39:28 -0500

    Upload

--- src/function.py
+++ src/function.py
@@ -5,32 +5,8 @@ import os
 import random
 from tqdm import tqdm
 import csv
-import traceback
 from llmtuner.data.template import get_template_and_fix_tokenizer
-# from llmtuner import ChatModel
-from llmtuner.extras.misc import torch_gc
-from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
-LOGFILE='./evaloutput.log'
-BATCH_SIZE=10
-
-# if os.path.exists(LOGFILE):
-#     # Remove the file
-#     os.remove(LOGFILE)
-#     print(f"The file {LOGFILE} has been removed.")
-# else:
-#     print(f"The file {LOGFILE} does not exist.")
-# # rouge = Rouge()
-# logging.basicConfig(
-#     format='%(asctime)s %(levelname)-4s - %(filename)-6s:%(lineno)d - %(message)s',
-#     level=logging.INFO,
-#     filename=LOGFILE,
-#     datefmt='%m-%d %H:%M:%S')
-# logging.info(f'Logger start: {os.uname()[1]}')
-
-# # tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
-# # tokenizer.pad_token = "[PAD]"
-# # tokenizer.padding_side = "left"
 def construct_prompt(template_name, history, summary):
     tokenizer = AutoTokenizer.from_pretrained(os.environ['CHATLM'], padding_side="left")
     
@@ -46,7 +22,7 @@ def construct_prompt(template_name, history, summary):
 def main():
     import json
     history = [['User', 'Hello, I need a ride to metro center, can somebody help me.'], ['Dispatcher', 'NCR Test Of the NCR Centers - Non Emergency'], ['User', 'Test received']]
-    print(construct_prompt('user',history, ''))
+    print(construct_prompt('user',history, 'Yiren summary'))
 
 
           
