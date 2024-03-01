ded671111f673dc9c7af501bdd1316987befa595
Yerong Li <yerong.li@outlook.com>
2024-03-01 12:01:05 -0600

    Print ans

---
+++ print_ans.py
@@ -0,0 +1,22 @@
+import argparse
+
+import json
+from tqdm import tqdm
+import random
+
+parser = argparse.ArgumentParser(description='Your program description')
+parser.add_argument('filename', type=argparse.FileType('r'))
+args = parser.parse_args()
+output_file_path = args.filename.name
+
+
+with open(output_file_path, "r") as file:
+    progress = [json.loads(line) for line in file]
+event_types = ['SuspiciousActivity', 'AccidentTrafficParking', 'DrugsAlcohol', 'EmergencyMessage', 'FacilitiesMaintenance', 'HarassmentAbuse', 'MentalHealth', 'NoiseDisturbance', 'TheftLostItem']
+
+for target_event_type in event_types:
+    for line in progress:
+        if line['type'] == target_event_type:
+            if 'response' in line:
+                print(line['prompt'], line['response'])
+                print(line['output'])
