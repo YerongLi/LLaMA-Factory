3d52c5f5526a6a1b55ad3b31e238d3d80829baf7
Yerong Li <yerong.li@outlook.com>
2024-03-01 13:22:24 -0600

    Upload

--- print_ans.py
+++ print_ans.py
@@ -16,7 +16,7 @@ data = []
 with open(output_file_path, "r") as file:
     progress = [json.loads(line) for line in file]
 # event_types = ['SuspiciousActivity', 'AccidentTrafficParking', 'DrugsAlcohol', 'EmergencyMessage', 'FacilitiesMaintenance', 'HarassmentAbuse', 'MentalHealth', 'NoiseDisturbance', 'TheftLostItem']
-event_types = ['AccidentTrafficParking']
+event_types = ['DrugsAlcohol']
 
 for target_event_type in event_types:
     for line in progress:
