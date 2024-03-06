31c47001e32e22c9b4c5a2a5e31569f75f777b05
Yerong Li <yerong.li@outlook.com>
2024-03-06 16:42:55 -0600

    Upload case_study files

---
+++ case_study.py
@@ -0,0 +1,22 @@
+import argparse
+
+import json
+from tqdm import tqdm
+import random
+import hashlib
+
+parser = argparse.ArgumentParser(description='Your program description')
+parser.add_argument('filename', type=argparse.FileType('r'))
+args = parser.parse_args()
+with open(file_name, "r") as file:
+    data = [json.loads(line) for line in file]
+
+# Filter entries where entry['o'] == 0 and entry['r'] == 1
+filtered_data = [entry for entry in data if entry.get('o') == 0 and entry.get('r') == 1]
+
+# Shuffle the filtered data
+random.shuffle(filtered_data)
+
+# Print the ['o'] and ['r'] values for each entry
+for entry in filtered_data:
+    print("['o']: {}, ['r']: {}".format(entry['o'], entry['r']))
