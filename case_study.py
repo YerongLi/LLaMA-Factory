2cf21ccb6fa74893ef38b138cfee07379e9c2996
Yerong Li <yerong.li@outlook.com>
2024-03-06 21:58:09 -0600

    Read filename arguments

--- case_study.py
+++ case_study.py
@@ -10,8 +10,7 @@ parser = argparse.ArgumentParser(description='Your program description')
 parser.add_argument('filename', type=argparse.FileType('r'))
 args = parser.parse_args()
 parser = argparse.ArgumentParser(description='Your program description')
-parser.add_argument('filename', type=argparse.FileType('r'))
-args = parser.parse_args()
+file_name = args.filename
 with open(file_name, "r") as file:
     data = [json.loads(line) for line in file]
 
