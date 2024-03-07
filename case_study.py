704b04ee741776c50a2ac81f827c3b99735b5dc9
Yerong Li <yerong.li@outlook.com>
2024-03-06 22:02:45 -0600

    Read filename arguments

--- case_study.py
+++ case_study.py
@@ -15,11 +15,11 @@ with open(file_name, "r") as file:
     data = [json.loads(line) for line in file]
 
 # Filter entries where entry['o'] == 0 and entry['r'] == 1
-filtered_data = [entry for entry in data if entry.get('o') == 0 and entry.get('r') == 1]
+filtered_data = [entry for entry in data if entry.get('o') != 1 and entry.get('r') == 1]
 
 # Shuffle the filtered data
 random.shuffle(filtered_data)
 
 # Print the ['o'] and ['r'] values for each entry
 for entry in filtered_data:
-    print("['o']: {}, ['r']: {}".format(entry['o'], entry['r']))
+    print("['output']: {}, ['response']: {}".format(entry['output'], entry['response']))
