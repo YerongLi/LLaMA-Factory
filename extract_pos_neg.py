857e82e7a8bf795b21cf3b3cefd264073cee1ea5
Yerong Li <yerong.li@outlook.com>
2024-03-13 15:24:29 -0500

    Extract

--- extract_pos_neg.py
+++ extract_pos_neg.py
@@ -28,9 +28,9 @@ def extract_keywords(sentence):
     return positive_keywords, negative_keywords
 with open('user4_w_key.jsonl', 'r') as count_file:
     total_lines = sum(1 for _ in count_file)
-with open('user4_w_key1.jsonl', 'r') as jsonl_file:
+with open('user4_w_key.jsonl', 'r') as jsonl_file:
     # Open the output file
-    with open('user4_w_key.jsonl', 'a') as output_file:
+    with open('user4_w_key1.jsonl', 'a') as output_file:
         # Iterate through each line in the input file
         for line in tqdm.tqdm(jsonl_file):
             # Parse JSON from the line
