65049d99b57ac4b2879069ceee56dc5e51de96a8
Yerong Li <yerong.li@outlook.com>
2024-03-17 21:23:21 -0500

    Upload

---
+++ sum.sh
@@ -0,0 +1,5 @@
+python src/cli_summary.py \
+    --model_name_or_path /scratch/yerong/.cache/pyllama/Llama-2-7b-hf \
+    --template dispatcher \
+    --finetuning_type lora \
+    --quantization_bit 4 
