97c9281a91f8142d40fa5e6cba44df7261a1efef
Yerong Li <yerong.li@outlook.com>
2024-03-20 15:16:29 -0500

    grammar prediction

--- grammar_prediction.py
+++ grammar_prediction.py
@@ -14,41 +14,41 @@ tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 max_length = 256
 
 def classify_texts(texts):
-	print(texts)
-    tokenized = tokenize_texts(texts, tokenizer, max_length)
-    input_ids = tokenized['input_ids'].to(device)
-    attention_mask = tokenized['attention_mask'].to(device)
+	print(text)
+	tokenized = tokenize_texts(texts, tokenizer, max_length)
+	input_ids = tokenized['input_ids'].to(device)
+	attention_mask = tokenized['attention_mask'].to(device)
 
-    with torch.no_grad():
-        logits = model(input_ids, attention_mask)
-        _, predicted = torch.max(logits, 1)
-        predicted_labels = predicted.tolist()
-        predicted_error_types = [list(error_type_to_index.keys())[label] for label in predicted_labels]
+	with torch.no_grad():
+		logits = model(input_ids, attention_mask)
+		_, predicted = torch.max(logits, 1)
+		predicted_labels = predicted.tolist()
+		predicted_error_types = [list(error_type_to_index.keys())[label] for label in predicted_labels]
 
-    return predicted_error_types
+	return predicted_error_types
 
 with open('user4_w_key.jsonl', 'r') as jsonl_file:
-    texts = []
-    for line in tqdm(jsonl_file):
-        json_obj = json.loads(line)
-        if 'response' not in json_obj:
-            continue
+	texts = []
+	for line in tqdm(jsonl_file):
+		json_obj = json.loads(line)
+		if 'response' not in json_obj:
+			continue
 
-        text = json_obj['o']
-        texts.append(text)
+		text = json_obj['o']
+		texts.append(text)
 
-        if len(texts) == 64:
-            predicted_error_types = classify_texts(texts)
-            for text, error_type in zip(texts, predicted_error_types):
-                print(f"Input: {text}")
-                print(f"Predicted Error Type: {error_type}")
-                print("-" * 50)
-            texts = []
+		if len(texts) == 64:
+			predicted_error_types = classify_texts(texts)
+			for text, error_type in zip(texts, predicted_error_types):
+				print(f"Input: {text}")
+				print(f"Predicted Error Type: {error_type}")
+				print("-" * 50)
+			texts = []
 
-    # Process remaining texts
-    if texts:
-        predicted_error_types = classify_texts(texts)
-        for text, error_type in zip(texts, predicted_error_types):
-            print(f"Input: {text}")
-            print(f"Predicted Error Type: {error_type}")
-            print("-" * 50)
+	# Process remaining texts
+	if texts:
+		predicted_error_types = classify_texts(texts)
+		for text, error_type in zip(texts, predicted_error_types):
+			print(f"Input: {text}")
+			print(f"Predicted Error Type: {error_type}")
+			print("-" * 50)
