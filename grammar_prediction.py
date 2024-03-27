8df9d021c7be4d91aeb6a99ecdba6b49a2d2b558
Yerong Li <yerong.li@outlook.com>
2024-03-27 15:46:59 -0500

    Upload

--- grammar_prediction.py
+++ grammar_prediction.py
@@ -1,3 +1,45 @@
+from grammar import RobertaClassifier
+import json
+from tqdm import tqdm
+error_type_to_index = {
+	'Sentence Structure Errors': 0,
+	'Verb Tense Errors': 1,
+	'Subject-Verb Agreement': 2,
+	'Article Usage': 3,
+	'Spelling Mistakes': 4,
+	'Preposition Usage': 5,
+	'Punctuation Errors': 6,
+	'Relative Clause Errors': 7,
+	'Gerund and Participle Errors': 8,
+	'Abbreviation Errors': 9,
+	'Slang, Jargon, and Colloquialisms': 10,
+	'Negation Errors': 11,
+	'Incorrect Auxiliaries': 12,
+	'Ambiguity': 13,
+	'Tautology': 14,
+	'Lack of Parallelism in Lists or Series': 15,
+	'Mixed Metaphors/Idioms': 16,
+	'Parallelism Errors': 17,
+	'Contractions Errors': 18,
+	'Conjunction Misuse': 19,
+	'Inappropriate Register': 20,
+	'Passive Voice Overuse': 21,
+	'Mixed Conditionals': 22,
+	'Faulty Comparisons': 23,
+	'Agreement in Comparative and Superlative Forms': 24,
+	'Ellipsis Errors': 25,
+	'Infinitive Errors': 26,
+	'Quantifier Errors': 27,
+	'Clichés': 28,
+	'Pronoun Errors': 29,
+	'Modifiers Misplacement': 30,
+	'Run-on Sentences': 31,
+	'Word Choice/Usage': 32,
+	'Sentence Fragments': 33,
+	'Capitalization Errors': 34,
+	'Redundancy/Repetition': 35,
+	'No Error': 36
+}
 from tqdm import tqdm
 import torch
 import matplotlib.pyplot as plt
