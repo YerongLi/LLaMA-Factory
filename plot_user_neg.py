c88646173a8c7418a604a94b3d92b029e479ffcc
Yerong Li <yerong.li@outlook.com>
2024-03-28 00:30:08 -0500

    Upload

--- plot_user_neg.py
+++ plot_user_neg.py
@@ -53,6 +53,8 @@ df = pd.DataFrame({
 sns.set(style="whitegrid")
 sns.histplot(data=df, x="Ratio", hue="Victim", palette={'Human neg': 'blue', 'LM neg': 'red', 'LM GAN neg': 'green', 'LM GPT-3.5 neg': 'brown'}, multiple="dodge", bins=5, element="bars", shrink=0.7)
 
+plt.xticks(ticks=[i * 0.2 for i in range(6)], labels=[f'{i * 0.2:.1f}' for i in range(6)])
+
 plt.xlabel('Ratio')
 plt.ylabel('Count')
 plt.title('Distribution of human, Llama, and Llama+GAN responses')
