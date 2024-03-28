d32d0025a4534c6d8920530c435964b95c57ab57
Yerong Li <yerong.li@outlook.com>
2024-03-27 22:10:31 -0500

    useroriginal_w_key.jsonl

--- plot_user_neg.py
+++ plot_user_neg.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='Your program description')
 parser.add_argument('filename', type=argparse.FileType('r'))
 args = parser.parse_args()
 file_name = args.filename.name
+
 # Read data from "summary.jsonl"
 event_id_key_dict = {}
 with open("summary_w_key.jsonl", "r") as jsonl_file:
@@ -17,21 +18,32 @@ with open("summary_w_key.jsonl", "r") as jsonl_file:
 
         if event_id:
             event_id_key_dict[event_id] = key_value
+
+# Read data from the provided file
 with open(file_name, "r") as file:
     data = [json.loads(line) for line in file]
 
-# Extract data for plotting
+# Extract data for plotting from the provided file
 zero_ratio = [len(line['history']) / event_id_key_dict[line['event_id']] for line in data if line['o'] == -1 and len(line['history']) / event_id_key_dict[line['event_id']] < 1.0+2e-9]
 r_ratio = [len(line['history']) / event_id_key_dict[line['event_id']] for line in data if line['r'] == -1 and len(line['history']) / event_id_key_dict[line['event_id']] < 1.0+2e-9]
+
+# Read data from "answer_gpt35.jsonl"
+with open("answer_gpt35.jsonl", "r") as jsonl_file:
+    answer_gpt35_data = [json.loads(line) for line in jsonl_file]
+
+# Extract data for plotting from "answer_gpt35.jsonl"
+r_ratio_gpt35 = [len(line['history']) / event_id_key_dict[line['event_id']] for line in answer_gpt35_data if line['r'] == -1 and len(line['history']) / event_id_key_dict[line['event_id']] < 1.0+2e-9]
 
 # Plotting
 plt.figure(figsize=(10, 6))
 
-plt.hist(zero_ratio, bins=30, alpha=0.5, color='blue', label ='Human neg')
+plt.hist(zero_ratio, bins=30, alpha=0.5, color='blue', label='Human neg')
 plt.hist(r_ratio, bins=30, alpha=0.5, color='red', label='LM neg')
+plt.hist(r_ratio_gpt35, bins=30, alpha=0.5, color='green', label='LM GPT-3.5 neg')
 
 plt.xlabel('Ratio')
 plt.ylabel('Count')
 plt.title('Distribution of human and LLAMA responses')
 plt.legend(loc='upper right')
 plt.savefig('distribution.png')  # Save the plot as 'distribution.png'
+plt.show()
