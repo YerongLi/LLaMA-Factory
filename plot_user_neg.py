0ff80ff454de0f4d944857eb947953a6b164eb47
Yerong Li <yerong.li@outlook.com>
2024-03-27 22:24:27 -0500

    useroriginal_w_key.jsonl

--- plot_user_neg.py
+++ plot_user_neg.py
@@ -35,11 +35,18 @@ with open("answer_gpt35.jsonl", "r") as jsonl_file:
 # Extract data for plotting from "answer_gpt35.jsonl"
 r_ratio_gpt35 = [len(line['history']) / event_id_key_dict[line['event_id']] for line in answer_gpt35_data if line['r'] == -1 and len(line['history']) / event_id_key_dict[line['event_id']] < 1.0+2e-9]
 
+# Determine the bins for the histogram
+bins = [i / 10 for i in range(21)]
+
 # Plotting using Seaborn
-plt.figure(figsize=(10, 6))
-sns.histplot(zero_ratio, bins=30, color='blue', alpha=0.7, label='Human neg', stat='density', kde=True)
-sns.histplot(r_ratio, bins=30, color='red', alpha=0.7, label='LM neg', stat='density', kde=True)
-sns.histplot(r_ratio_gpt35, bins=30, color='brown', alpha=0.7, label='LM GPT-3.5 neg', stat='density', kde=True)
+plt.figure(figsize=(12, 8))
+
+sns.histplot(zero_ratio, bins=bins, color='blue', alpha=0.7, label='Human neg', stat='density', kde=True)
+sns.histplot(r_ratio, bins=bins, color='red', alpha=0.7, label='LM neg', stat='density', kde=True)
+sns.histplot(r_ratio_gpt35, bins=bins, color='brown', alpha=0.7, label='LM GPT-3.5 neg', stat='density', kde=True)
+
+# Set x-axis tick labels
+plt.xticks([i / 10 for i in range(11)], [f'{i / 10:.1f}' for i in range(11)])
 
 plt.xlabel('Ratio')
 plt.ylabel('Density')
