d1002e74931004e02741c85a7114a12c8e927060
Yerong Li <yerong.li@outlook.com>
2024-03-28 00:28:45 -0500

    Upload

--- plot_user_neg.py
+++ plot_user_neg.py
@@ -51,7 +51,7 @@ df = pd.DataFrame({
 
 # Plotting with Seaborn
 sns.set(style="whitegrid")
-sns.histplot(data=df, x="Ratio", hue="Victim", palette={'Human neg': 'blue', 'LM neg': 'red', 'LM GAN neg': 'green', 'LM GPT-3.5 neg': 'brown'}, multiple="dodge", bins=5, element="bars")
+sns.histplot(data=df, x="Ratio", hue="Victim", palette={'Human neg': 'blue', 'LM neg': 'red', 'LM GAN neg': 'green', 'LM GPT-3.5 neg': 'brown'}, multiple="dodge", bins=5, element="bars", shrink=0.7)
 
 plt.xlabel('Ratio')
 plt.ylabel('Count')
