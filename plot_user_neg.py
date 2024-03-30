741ba602694e03453a91dbdab9cf4ae711446754
Yerong Li <yerong.li@outlook.com>
2024-03-29 20:19:14 -0500

    hatches

--- plot_user_neg.py
+++ plot_user_neg.py
@@ -108,7 +108,7 @@ for container, hatch, handle in zip(ax.containers, hatches, ax.get_legend().lege
 plt.xticks(ticks=[i for i in range(5)], labels=[f'{(i +1)* 20}%' for i in range(5)])
 # plt.legend(title='Model')
 # ax.legend_.remove() # remove the automatic legends
-ax.legend( loc='upper left', bbox_to_anchor=[0.01, 1.01])
+plt.legend( loc='upper left', bbox_to_anchor=[0.01, 1.01])
 # ax.legend( loc='upper right')
 
 plt.xlabel('Stages of Dialogue')
