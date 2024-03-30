2e69412a9eedc4f54935af04ec2d60b298f6b636
Yerong Li <yerong.li@outlook.com>
2024-03-29 19:28:01 -0500

    hatches

--- plot_user_neg.py
+++ plot_user_neg.py
@@ -48,7 +48,7 @@ r_ratio_usergan = [len(line['history']) / event_id_key_dict[line['event_id']] fo
 
 df = pd.DataFrame({
     'Ratio': zero_ratio + r_ratio + r_ratio_usergan + r_ratio_gpt35,
-    'Victim': ['Human'] * len(zero_ratio) + ['VicSim'] * len(r_ratio_usergan) + ['VicSim w/o GAN'] * len(r_ratio) + ['GPT3.5'] * len(r_ratio_gpt35),
+    'Model': ['Human'] * len(zero_ratio) + ['VicSim'] * len(r_ratio_usergan) + ['VicSim w/o GAN'] * len(r_ratio) + ['GPT3.5'] * len(r_ratio_gpt35),
     'total': [1] * len(zero_ratio) + [1] * len(r_ratio_usergan) + [1] * len(r_ratio) + [1] * len(r_ratio_gpt35)
 })
 print(df)
@@ -77,7 +77,7 @@ df.drop(columns=['Ratio'], inplace=True)  # Drop the original 'Ratio' column
 # sns.set(style="whitegrid")
 # ax = sns.histplot(data=df, x="Ratio", hue="Victim", palette={'Human': 'lightblue', 'VicSim': 'grey', 'VicSim w/o GAN': 'lightgreen', 'GPT3.5': 'salmon'}, multiple="dodge", bins=5, element="bars", shrink=0.6)
 
-ax = sns.barplot(data=df, x="Ratio_Group", y="total", hue="Victim")
+ax = sns.barplot(data=df, x="Ratio_Group", y='total', hue='Model', estimator=sum)
 
 hatches = itertools.cycle(['/', '\\', 'o', '*'])
 # hatches = itertools.cycle(['/', '//', '+', '-', 'x', '\\', '*', 'o', 'O', '.'])
