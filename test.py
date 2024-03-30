641e066d780f68c87e5fcf393dfe5b0d4ab0c258
Yerong Li <yerong.li@outlook.com>
2024-03-29 19:04:10 -0500

    Update colors

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-import json
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-json_data = '''
-[
-  {"Dispatcher": "Thank you for using Live Safe...What is your location please?"},
-  {"User": "### Blackstone apt ##"}
-]
-'''
+tips = sns.load_dataset("tips")
 
-parsed_data = json.loads(json_data)
-print(parsed_data)
+hatches = ['\\\\', '//']
+
+fig, ax = plt.subplots(figsize=(6,3))
+print(tips.head())
+sns.barplot(data=tips, x="day", y="total_bill", hue="time")
+
+# loop through days
+for hues, hatch in zip(ax.containers, hatches):
+    # set a different hatch for each time
+    for hue in hues:
+        hue.set_hatch(hatch)
+
+# add legend with hatches
+plt.legend().loc='best'
+
+plt.show()
