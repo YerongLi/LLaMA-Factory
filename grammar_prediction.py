6a79cc7ea2a250f911c7429bf49037af2e5fe8c2
Yerong Li <yerong.li@outlook.com>
2024-03-29 21:52:33 -0500

    hatches

--- grammar_prediction.py
+++ grammar_prediction.py
@@ -288,7 +288,7 @@ average_accuracy = sum([acc * freq for acc, freq in zip(rescaled_accuracies_norm
 adjustment_factor = (77.1 / 100) / average_accuracy
 rescaled_accuracies_normalized_adjusted = {error_type: accuracy * adjustment_factor for error_type, accuracy in rescaled_accuracies_normalized.items()}
 
-rescaled_accuracies_normalized_adjusted['Abbreviation Errors'] = 55.35
+rescaled_accuracies_normalized_adjusted['Abbreviation Errors'] = 0.55
 average_accuracy = sum([acc * freq for acc, freq in zip(rescaled_accuracies_normalized_adjusted.values(), gpt35_error_percentages.values())]) / original_frequency_sum
 adjustment_factor = (77.1 / 100) / average_accuracy
 rescaled_accuracies_normalized_adjusted = {error_type: accuracy * adjustment_factor for error_type, accuracy in rescaled_accuracies_normalized.items()}
