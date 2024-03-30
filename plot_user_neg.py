479a7e1312845970d633cc065a77e81348580fb0
Yerong Li <yerong.li@outlook.com>
2024-03-29 19:30:18 -0500

    hatches

--- plot_user_neg.py
+++ plot_user_neg.py
@@ -76,7 +76,7 @@ df['Ratio_Group'] = df['Ratio'].apply(map_to_ratio_group)
 df.drop(columns=['Ratio'], inplace=True)  # Drop the original 'Ratio' column
 # sns.set(style="whitegrid")
 # ax = sns.histplot(data=df, x="Ratio", hue="Victim", palette={'Human': 'lightblue', 'VicSim': 'grey', 'VicSim w/o GAN': 'lightgreen', 'GPT3.5': 'salmon'}, multiple="dodge", bins=5, element="bars", shrink=0.6)
-fig, ax = plt.subplots(figsize=(6,3))
+fig, ax = plt.subplots()
 sns.barplot(data=df, x="Ratio_Group", y='total', hue='Model', estimator=sum)
 
 hatches = itertools.cycle(['/', '\\', 'o', '*'])
